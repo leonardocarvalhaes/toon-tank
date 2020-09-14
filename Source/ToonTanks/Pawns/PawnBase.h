38b113deaa73a0f4f0be145d07f8ce33a4f7c7a3
Leonardo Carvalhães Bernardo <leonardocarvalhaes@hotmail.com>
2020-09-13 22:48:25 -0300

    I had to reinitialize the project from the benning and I followed the course classes until the tank movement to be implemented.

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,37 +5,25 @@
 #include "PawnBase.generated.h"
 
 class UCapsuleComponent;
-class AProjectileBase;
 
 UCLASS()
 class TOONTANKS_API APawnBase : public APawn
 {
 	GENERATED_BODY()
 
-private: 
-	// COMPONENTS
+private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UCapsuleComponent* CapsuleComp;
+	UCapsuleComponent* CapsuleComp = nullptr;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* BaseMesh;
+	UStaticMeshComponent* BaseMesh = nullptr;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* TurretMesh;
+	UStaticMeshComponent* TurretMesh = nullptr;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	USceneComponent* ProjectileSpawnPoint;
-	// VARIABLES
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile Type", meta = (AllowPrivateAccess = "true"))
-	TSubclassOf<AProjectileBase> ProjectileClass;
+	USceneComponent* ProjectileSpawnPoint = nullptr;
 
 public:
-	// Sets default values for this pawn's properties
 	APawnBase();
-
-protected:
-
-	void RotateTurret(FVector LookAtTarget);
-
-	void Fire();
-
-	virtual void HandleDestruction();
-
 };
--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -15,20 +13,20 @@ class TOONTANKS_API APawnTank : public APawnBase
 	GENERATED_BODY()
 
 private:
-	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* SpringArm;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UCameraComponent* Camera; 
+	UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float MoveSpeed = 500.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float RotationSpeed = 250.f;
 
 	FVector MoveDirection;
 	FQuat RotationDirection;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float MoveSpeed = 100.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))	
-	float RotateSpeed = 100.0f;
-
-	APlayerController* PlayerControllerRef;
 
 	void CalculateMoveInput(float Value);
 	void CalculateRotateInput(float Value);
@@ -37,19 +35,15 @@ private:
 	void Rotate();
 
 public:
-
 	APawnTank();
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-	virtual void HandleDestruction() override; 
-
 };
